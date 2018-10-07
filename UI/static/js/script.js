fa70cda885795b9f06e597e7d6d18b94b2f1f94a
kwanj-k <mwangikelvin23@.com>
2018-10-07 19:27:21 +0300

    Added an add to cart helper modal [#161036725]

--- UI/static/js/script.js
+++ UI/static/js/script.js
@@ -2,27 +2,23 @@ function b_show() {
     alert('Hello\nOur blog does not have posts yet.');
 }
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("green")[0];
+let done = document.getElementsByClassName("green")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
+function adc() {
+    modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+done.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
