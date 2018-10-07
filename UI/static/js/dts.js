b20f8a3c566cd4c0b8f3d432cfeaf22a26133646
kwanj-k <mwangikelvin23@.com>
2018-10-07 20:51:43 +0300

    Added a products details modal [finishes #161036826]

---
+++ UI/static/js/dts.js
@@ -0,0 +1,6 @@
+
+let dets  = document.getElementById('dets2');
+function dts() {
+    dets.style.display = "block";
+}
+
--- UI/static/js/script.js
+++ UI/static/js/script.js
@@ -3,21 +3,16 @@ function b_show() {
 }
 // Get the modal
 let modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 let done = document.getElementsByClassName("green")[0];
 
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 function adc() {
     modal.style.display = "block";
 }
-// When the user clicks on <span> (x), close the modal
+
 done.onclick = function() {
     modal.style.display = "none";
 }
