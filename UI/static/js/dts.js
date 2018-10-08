afb324a123f052b7610d1ce39faed17e761a7493
kwanj-k <mwangikelvin23@.com>
2018-10-08 21:12:33 +0300

    [Feature #161036863]Add an add and update form modal

--- UI/static/js/dts.js
+++ UI/static/js/dts.js
@@ -4,3 +4,8 @@ function dts() {
     dets.style.display = "block";
 }
 
+// Get the <span> element that closes the modal
+let done = document.getElementsByClassName("green")[0];
+done.onclick = function() {
+    modal.style.display = "none";
+}
--- UI/static/js/script.js
+++ UI/static/js/script.js
@@ -1,16 +1,36 @@
 function b_show() {
     alert('Hello\nOur blog does not have posts yet.');
 }
+
+function del() {
+    alert('Product Deleted!');
+}
 // Get the modal
 let modal = document.getElementById('myModal');
-let done = document.getElementsByClassName("green")[0];
 
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let done = document.getElementByClassName("green");
+let add = document.getElementById("add");
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 function adc() {
     modal.style.display = "block";
 }
-
+// When the user clicks on <span> (x), close the modal
 done.onclick = function() {
     modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 
