70096ccb9f8dbc8468bc2e722bdca4b1e1e60a69
kwanj-k <mwangikelvin23@.com>
2018-10-09 07:57:28 +0300

    [#161037020] Going live

--- UI/static/js/t.js
+++ UI/static/js/t.js
@@ -3,11 +3,9 @@ function sell() {
 }
 let edit = document.getElementById('editp');
 let update = document.getElementsByClassName('update')[0];
-
 function editf() {
     edit.style.display = "block";
 }
-
 update.onclick = function() {
     edit.style.display = "none";
 }
