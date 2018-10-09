97cbc26ea7542fadcf9d7876ba8c74f9dd03f535
kwanj-k <mwangikelvin23@.com>
2018-10-09 18:51:24 +0300

    [Finishes #161088393] Fixed all the bugs from review Add title on products page fix product details modal Add back button on mycarts fix all js

--- UI/static/js/script.js
+++ UI/static/js/script.js
@@ -5,6 +5,9 @@ function b_show() {
 function del() {
     alert('Product Deleted!');
 }
+function sell() {
+    alert('Sold!');
+}
 function user_add() {
     alert('Attendant Added!');
 }
@@ -12,13 +15,15 @@ function user_add2() {
     alert('Admin Added!');
 }
 // Get the modal
-let modal = document.getElementById('myModal');
+let modal = document.getElementById('addtocart');
+let mod2 = document.getElementById('details');
 
 // Get the button that opens the modal
 let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-let done = document.getElementByClassName("green");
+let done = document.getElementsByClassName("added-to-cart")[0];
+let don2 = document.getElementsByClassName("viewed")[0];
 let add = document.getElementById("add");
 
 // When the user clicks on the button, open the modal 
@@ -28,9 +33,15 @@ btn.onclick = function() {
 function adc() {
     modal.style.display = "block";
 }
+function details() {
+    mod2.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 done.onclick = function() {
     modal.style.display = "none";
+}
+don2.onclick = function() {
+    mod2.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
