551fb2bc9b8cdf437110dbae9a77a46720a70333
Kelvin.Mwangi <33450849+kwanj-k@users.noreply.github.com>
2018-10-24 07:16:18 +0300

    Add a static storemanager UI
    * Added a navigation bar[#161036675]
    
    * Added home content and a background[#161036675]
    
    * Added a responsive footer[finishes #161036675]
    
    * Added a create store form [#161036702]
    
    * Made the create store page responsive [#161036702]
    
    * Made a responsive login page [finishes #161036702]
    
    * Added a products page [#161036808]
    
    * Added a responsive search bar [finishes #161036808]
    
    * made a modal for adding to cart
    
    * Added an add to cart helper modal [#161036725]
    
    * Made modal responsive [finishes #161036725]
    
    * Added a products details modal [finishes #161036826]
    
    * Added a user profile page [#161036933]
    
    * Made profile responsive [finishes #161036933]
    
    * Added a sales record page [#161036783]
    
    * Added a sales record page responsive[finishes #161036783]
    
    * Added an edit profile page [#161044744]
    
    * Added javascript to make edit-profile page into a modal [ finishes #161044744]
    
    * Added a page for admin to see all products and details[#161036863]
    
    * [Feature #161036863]Add an add and update form modal
    
    * [Finishes #161036863] Make the page responsive
    
    * [Feature #161036870] Add an add attendant form
    
    * [Finishes #161036870] Make page responsive
    Add alert to adding attendant and admin
    
    * Fix anchor tags
    
    * [Finishes #161036910] Add sale records page
    
    * [Bug #161075174] Remove lorem
    Add login form on the landing page
    
    * [Bug #161075174] Remove lorem and fix background
    Add a carts/mycarts page
    
    * [Finishes #161075174] Add newline at end of files
    
    * [#161037020] Going live
    
    * [Finishes #161076586] Add readme
    
    * [Finishes #161088393] Fixed all the bugs from review
    Add title on products page
    fix product details modal
    Add back button on mycarts
    fix all js
    
    * [Finishes #161037020] Deploy to gh-pages
    
    * [Finishes #161156714] Fix all broken links and buttons
    
    * [Finishes #161177569] Fix the admin/attendant site link
    
    * [Bug #161390577] Fix background and styles
    change background to use color
    aligned login buttons
    pushed landing page content down
    
    * [Finishes #161390577] Add images to readme
    
    * [Finishes #161390577] Add images to readme
    
    * [Finishes #161390577] Add images to readme

---
+++ UI/static/js/t.js
@@ -0,0 +1,14 @@
+function sell() {
+    alert('Sold!');
+}
+let edit = document.getElementById('editp');
+let update = document.getElementsByClassName('update')[0];
+function editf() {
+    edit.style.display = "block";
+}
+update.onclick = function() {
+    edit.style.display = "none";
+}
+function b_show() {
+    alert('Hello\nOur blog does not have posts yet.');
+}
