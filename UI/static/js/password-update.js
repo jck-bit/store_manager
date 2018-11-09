6b36d3902b6d2e788df2ddb65fa459c2f966240b
kwanj-k <mwangikelvin23@.com>
2018-11-09 11:27:30 +0300

    [Feature #161833467] Edit password

---
+++ UI/static/js/password-update.js
@@ -0,0 +1,40 @@
+//Update password
+
+let passUpdate = document.getElementById('passupdate');
+
+ if (passUpdate){
+    passUpdate.addEventListener
+    ('submit', passwordUpdate);
+}
+function passwordUpdate(e){
+    e.preventDefault();
+    let passwordUrl = `https://storemanager-v2.herokuapp.com/api/v2/editpassword`;
+    const token = window.localStorage.getItem('token');
+    let oldpass = document.getElementById('oldpass').value;
+    let newpass = document.getElementById('newpass').value;
+    fetch(passwordUrl, {
+        method: 'PUT',
+        headers: {
+            'Content-type':'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        
+        body:JSON.stringify({"old_password":oldpass,
+                              "new_password":newpass})
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status === 'Failed!'){
+                // if request is unsuccessful
+                document.getElementById('output').style.color = 'red'
+                document.getElementById('output').innerHTML = data.message
+            }
+            if (data.status === "Updated!"){
+                // if request is successful
+                document.getElementById('output').style.color = 'green'
+                document.getElementById('output').innerHTML = data.status
+                
+            }
+    
+        })
+    }
